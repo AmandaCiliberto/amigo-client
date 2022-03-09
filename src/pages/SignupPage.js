a425ce65eb5bb454eede46df74ad285887599e2f
Amanda Rodrigues <amanda.rodrigues@lego.com>
2022-03-09 09:03:19 +0100

    fourth try to fix API url

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -4,9 +4,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const API_URL = "https://amigo-application.herokuapp.com/";
-
-
+const API_URL = "https://amigo-application.herokuapp.com";
 
 function SignupPage(props) {
   const [email, setEmail] = useState("");
